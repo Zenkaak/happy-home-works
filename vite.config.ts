124d5159e09528aa049157473cecfb5cc225a55f
Zenkaak <jaykibo736@gmail.com>
2026-04-19 21:25:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   build: {
     outDir: "dist",
     emptyOutDir: true,
   },
-
-  // IMPORTANT: ensures SPA routing works in production
-  preview: {
-    port: 4173,
-    strictPort: true,
-  },
-});
+}); 
